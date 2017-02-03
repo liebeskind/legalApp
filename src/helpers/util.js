8d178a02ebebc706b90a0b8729137f4c802f47ef
Rich Parrish <richparrishx@gmail.com>
2017-02-03 13:31:48 -0800

    better bindAll error messages

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react'
 
 export function bindAll(fns, ctx) {
-  fns.map((fn) => ctx[fn] = ctx[fn].bind(ctx));
+  fns.map((fn) => {
+    try {
+      ctx[fn] = ctx[fn].bind(ctx)
+    } catch(e) {
+      throw `bindAll error: ${fn} is not a function`;
+    }
+  });
 }
