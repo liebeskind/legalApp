3c434d7cad1b7924626cb633641f2299c01e4bb9
liebeskind <daniel.liebeskind@gmail.com>
2017-02-08 16:22:14 -0500

    Revamps pdf outputs so now more dynamic and outputs in correct bundles

--- src/helpers/pdf.js
+++ src/helpers/pdf.js
@@ -6,9 +6,9 @@ export function generatePDFBySignatory(signatory, loaded) {
   exportPDF(data);
 }
 
-export function generatePDF(loaded) {
+export function generatePDF(loaded, specificSignatory) {
   var data = formatPDF(loaded);
-  exportPDF(data);
+  exportPDF(data, specificSignatory);
 }
 
 function formatPDFBySignatory(signatory, loaded) {
@@ -23,8 +23,6 @@ function formatPDFBySignatory(signatory, loaded) {
   for (let doc in loaded.companiesPerDocument) {
 
     let documentData = loaded.companiesPerDocument[doc]
-    // console.log(documentData)
-    // console.log(loaded.companiesPerDocument[doc])
     for (let company in documentData) {
       let companyData = documentData[company]
       signatoryBundle[companyData.signatory] = signatoryBundle[companyData.signatory] || {};
@@ -32,8 +30,6 @@ function formatPDFBySignatory(signatory, loaded) {
       signatoryBundle[companyData.signatory][doc].push(company)
     }
   }
-
-  console.log(signatoryBundle);
 }
 
 function formatPDF(loaded) {
@@ -54,18 +50,95 @@ function formatPDF(loaded) {
 
       if (!signatory) {
         output[doc].groupedBySignatory['unassigned'] = output[doc].groupedBySignatory['unassigned'] || {};
-        output[doc].groupedBySignatory['unassigned'][company] = companyInfo;
+        output[doc].groupedBySignatory['unassigned'].companies = output[doc].groupedBySignatory['unassigned'].companies || {};
+        output[doc].groupedBySignatory['unassigned'].companies[company] = companyInfo;
       } else {
         output[doc].groupedBySignatory[signatory] = output[doc].groupedBySignatory[signatory] || {};
-        output[doc].groupedBySignatory[signatory].signatoryName = loaded.sigs[signatory].name;
-        output[doc].groupedBySignatory[signatory][company] = companyInfo;
-        output[doc].groupedBySignatory[signatory][company].signatoryTitle = loaded.officersOfCompany[company][signatory].title
+        output[doc].groupedBySignatory[signatory].companies = output[doc].groupedBySignatory[signatory].companies || {};
+        output[doc].groupedBySignatory[signatory].companies[company] = companyInfo;
+        output[doc].groupedBySignatory[signatory].companies[company].signatoryTitle = loaded.officersOfCompany[company][signatory].title
+        output[doc].groupedBySignatory[signatory].companies[company].signatoryName = loaded.sigs[signatory].name;
       }
     }
   }
 
-  console.log(output);
   return output;
+}
+
+function exportPDF(input, specificSignatory) {
+  // validation
+  // if(Array.isArray(input)) input = {sigs: input};
+
+  var defaults = {
+    company: '[COMPANY]',
+    name: '[NAME]',
+    title: '[TITLE]'
+  };
+
+  var verticalOffset;
+  var sigWidth = inch(3);
+  var spacing = 50;
+  var fontSize = 11;
+
+  var doc = new jsPDF({
+    unit: 'pt', // allows for relative spacing based on text inputs
+    format: [inch(8.5), inch(11)]
+  }).setFont('times').setFontSize(fontSize.toString());
+
+  // var groupedSigs = groupSigs(input.sigs);
+
+  Object.keys(input).forEach((key, docIndex, docArray) => {
+    var docOutput = input[key];
+  // })
+
+    Object.keys(docOutput.groupedBySignatory).forEach((key, i, arr) => {
+      if (specificSignatory && key != specificSignatory) return;
+      var companies = docOutput.groupedBySignatory[key].companies;
+      verticalOffset = inch(1.5);
+
+      // add boilerplate to first page
+      if(i === 0){
+        var boilerplate = doc.splitTextToSize(`\tIN WITNESS WHEREOF, each of the parties hereto has caused a counterpart ${docOutput.agreementType ? `of this ${docOutput.agreementType} ` : ``}to be duly executed and delivered as of the date first above written.`, inch(7));
+        doc.text(inch(0.5), inch(1), boilerplate);
+      }
+
+      // add document title to bottom of each page
+      var documentTitle = doc.splitTextToSize(`[Signature page to ${docOutput.footerTitle ? docOutput.footerTitle : "NAME OF DOCUMENT"}]`, inch(7));
+      var xOffset = ((doc.internal.pageSize.width - doc.getStringUnitWidth(documentTitle.toString()) * doc.internal.getFontSize()) / 2);
+      doc.text(documentTitle, xOffset, doc.internal.pageSize.height - inch(1));
+
+      // standardize grouped data
+      var copy;
+      var data = Object.keys(companies).map((value) => {
+        var item = companies[value]
+        copy = Object.assign({}, defaults);
+        return Object.assign(copy, item);
+      });
+
+      // add grouped data to page
+      var currentSig;
+      data.map((item, i) => {
+        // weird but necessary formatting
+        currentSig = doc.splitTextToSize(
+  `
+  ${item.companyName}${item.asField ? `,
+  as ${item.asField}
+  ` : `
+  `}
+  By:
+  Name:  ${item.signatoryName ? item.signatoryName : "[NAME]"}
+  Title:    ${item.signatoryTitle ? item.signatoryTitle : "[TITLE]"}`, sigWidth);
+
+        doc.text(inch(5), verticalOffset, currentSig);
+        verticalOffset += ((currentSig.length*fontSize) + spacing);
+      });
+
+      // Only adds a new page when there are more pages necessary
+      if(i*docIndex != arr.length) doc.addPage();
+    });
+  });
+
+  doc.save('exampleSignaturePagesBundle.pdf');
 }
 
 //Rich's version
@@ -108,83 +181,84 @@ function formatPDF(loaded) {
 //   return formatted;
 // }
 
-function exportPDF(input = { agreement: '[AGREEMENT]', footerTitle: '[NAME OF DOCUMENT]', sigs: [ {} ] }) {
-  // validation
-  // if(Array.isArray(input)) input = {sigs: input};
+//Rich's version
+// function exportPDF(input = { agreement: '[AGREEMENT]', footerTitle: '[NAME OF DOCUMENT]', sigs: [ {} ] }) {
+//   // validation
+//   // if(Array.isArray(input)) input = {sigs: input};
 
-  var defaults = {
-    company: '[COMPANY]',
-    name: '[NAME]',
-    title: '[TITLE]'
-  };
+//   var defaults = {
+//     company: '[COMPANY]',
+//     name: '[NAME]',
+//     title: '[TITLE]'
+//   };
 
-  var verticalOffset;
-  var sigWidth = inch(3);
-  var spacing = 50;
-  var fontSize = 11;
-  var sigs;
+//   var verticalOffset;
+//   var sigWidth = inch(3);
+//   var spacing = 50;
+//   var fontSize = 11;
+//   var sigs;
 
-  var doc = new jsPDF({
-    unit: 'pt', // allows for relative spacing based on text inputs
-    format: [inch(8.5), inch(11)]
-  }).setFont('times').setFontSize(fontSize.toString());
+//   var doc = new jsPDF({
+//     unit: 'pt', // allows for relative spacing based on text inputs
+//     format: [inch(8.5), inch(11)]
+//   }).setFont('times').setFontSize(fontSize.toString());
 
-  var groupedSigs = groupSigs(input.sigs);
+//   var groupedSigs = groupSigs(input.sigs);
 
-  Object.keys(groupedSigs).forEach((key, i, arr) => {
-    sigs = groupedSigs[key];
-    verticalOffset = inch(1.5);
+//   Object.keys(groupedSigs).forEach((key, i, arr) => {
+//     sigs = groupedSigs[key];
+//     verticalOffset = inch(1.5);
 
-    // add boilerplate to first page
-    if(i === 0){
-      var boilerplate = doc.splitTextToSize(`\tIN WITNESS WHEREOF, each of the parties hereto has caused a counterpart ${input.agreement ? `of this ${input.agreement} ` : ``}to be duly executed and delivered as of the date first above written.`, inch(7));
-      doc.text(inch(0.5), inch(0.5), boilerplate);
-    }
+//     // add boilerplate to first page
+//     if(i === 0){
+//       var boilerplate = doc.splitTextToSize(`\tIN WITNESS WHEREOF, each of the parties hereto has caused a counterpart ${input.agreement ? `of this ${input.agreement} ` : ``}to be duly executed and delivered as of the date first above written.`, inch(7));
+//       doc.text(inch(0.5), inch(0.5), boilerplate);
+//     }
 
-    // add document title to bottom of each page
-    var documentTitle = doc.splitTextToSize(`[Signature page to ${input.footerTitle}]`, inch(7));
-    var xOffset = ((doc.internal.pageSize.width - doc.getStringUnitWidth(documentTitle.toString()) * doc.internal.getFontSize()) / 2);
-    doc.text(documentTitle, xOffset, doc.internal.pageSize.height - 75);
+//     // add document title to bottom of each page
+//     var documentTitle = doc.splitTextToSize(`[Signature page to ${input.footerTitle}]`, inch(7));
+//     var xOffset = ((doc.internal.pageSize.width - doc.getStringUnitWidth(documentTitle.toString()) * doc.internal.getFontSize()) / 2);
+//     doc.text(documentTitle, xOffset, doc.internal.pageSize.height - 75);
 
-    // standardize grouped data
-    var copy;
-    var data = sigs.map((item) => {
-      copy = Object.assign({}, defaults);
-      return Object.assign(copy, item);
-    });
+//     // standardize grouped data
+//     var copy;
+//     var data = sigs.map((item) => {
+//       copy = Object.assign({}, defaults);
+//       return Object.assign(copy, item);
+//     });
 
-    // add grouped data to page
-    var currentSig;
-    data.map((item, i) => {
-      // weird but necessary formatting
-      currentSig = doc.splitTextToSize(
-`${item.company}${item.holdings ? `,
-as ${item.holdings}
-` : `
-`}
-By:
-Name:  ${item.name}
-Title:    ${item.title}`, sigWidth);
+//     // add grouped data to page
+//     var currentSig;
+//     data.map((item, i) => {
+//       // weird but necessary formatting
+//       currentSig = doc.splitTextToSize(
+// `${item.company}${item.holdings ? `,
+// as ${item.holdings}
+// ` : `
+// `}
+// By:
+// Name:  ${item.name}
+// Title:    ${item.title}`, sigWidth);
 
-      doc.text(inch(5), verticalOffset, currentSig);
-      verticalOffset += ((currentSig.length*fontSize) + spacing);
-    });
+//       doc.text(inch(5), verticalOffset, currentSig);
+//       verticalOffset += ((currentSig.length*fontSize) + spacing);
+//     });
 
-    // add page if more signatories
-    if(i < arr.length - 1) doc.addPage();
-  });
+//     // add page if more signatories
+//     if(i < arr.length - 1) doc.addPage();
+//   });
 
-  doc.save('exampleSignaturePagesBundle.pdf');
-}
+//   doc.save('exampleSignaturePagesBundle.pdf');
+// }
 
-function groupSigs(sigs = []) {
-  return sigs.reduce((acc, sig) => {
-    if(!sig.name){ sig.name = '[NAME]' }
-    acc[sig.name] = acc[sig.name] ||  []
-    acc[sig.name].push(sig);
-    return acc;
-  }, {});
-}
+// function groupSigs(sigs = []) {
+//   return sigs.reduce((acc, sig) => {
+//     if(!sig.name){ sig.name = '[NAME]' }
+//     acc[sig.name] = acc[sig.name] ||  []
+//     acc[sig.name].push(sig);
+//     return acc;
+//   }, {});
+// }
 
 function inch(x) {
   return x*72;
